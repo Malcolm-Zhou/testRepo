be0c600ce2add52437a7910bc4499c50be9653f2
Malcolm-Zhou <zmhdyx@gmail.com>
2017-11-09 14:50:36 +0800

    clean mess

--- a.js
+++
@@ -1,4 +0,0 @@
-let url = "/perform/mobile/huddle/state2";
-console.log(url.lastIndexOf('/'));
-let path = url.substring(url.indexOf('state') + 5, url.indexOf('state') + 7).toString();
-console.log(path);
