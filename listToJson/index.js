bcd48aaefae8c069a3bf9a2c0ef55f4b3d0731eb
Malcolm-Zhou <zmhdyx@gmail.com>
2018-05-06 19:27:49 +0800

    submit

---
+++ listToJson/index.js
@@ -0,0 +1,162 @@
+var express = require('express');
+var router = express.Router();
+
+
+console.log()
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+    let str = `attr1
+attr2
+attr3
+attr4
+attr5
+attr6
+attr7
+attr8
+`
+    let obj = strToObj(str)
+
+    let str2 = `attr9
+attr10`
+
+    let obj2 = strToObj(str2)
+    addInnerAttr(obj, 'attr3', obj2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    function strToObj(str) {
+        let obj = {}
+
+        let attrs = str.split('\n')
+
+
+
+        attrs.forEach(attr => {
+            if (attr) obj[attr] = ""
+        });
+
+
+        return obj
+    }
+
+
+
+
+
+
+    // let json = formatJson(obj)
+    let jsonHtml = formatJsonHtml(obj)
+
+    res.render('index', { title: 'Express', json: jsonHtml });
+});
+
+function addInnerAttr(outerObj, attrName, attrObj) {
+    outerObj[attrName] = attrObj
+}
+
+
+var formatJson = function(json, options) {
+    var reg = null,
+        formatted = '',
+        pad = 0,
+        PADDING = '    ';
+    options = options || {};
+    options.newlineAfterColonIfBeforeBraceOrBracket = (options.newlineAfterColonIfBeforeBraceOrBracket === true) ? true : false;
+    options.spaceAfterColon = (options.spaceAfterColon === false) ? false : true;
+    if (typeof json !== 'string') {
+        json = JSON.stringify(json);
+    } else {
+        json = JSON.parse(json);
+        json = JSON.stringify(json);
+    }
+    reg = /([\{\}])/g;
+    json = json.replace(reg, '\r\n$1\r\n');
+    reg = /([\[\]])/g;
+    json = json.replace(reg, '\r\n$1\r\n');
+    reg = /(\,)/g;
+    json = json.replace(reg, '$1\r\n');
+    reg = /(\r\n\r\n)/g;
+    json = json.replace(reg, '\r\n');
+    reg = /\r\n\,/g;
+    json = json.replace(reg, ',');
+    if (!options.newlineAfterColonIfBeforeBraceOrBracket) {
+        reg = /\:\r\n\{/g;
+        json = json.replace(reg, ':{');
+        reg = /\:\r\n\[/g;
+        json = json.replace(reg, ':[');
+    }
+    if (options.spaceAfterColon) {
+        reg = /\:/g;
+        json = json.replace(reg, ':');
+    }
+    (json.split('\r\n')).forEach(function(node, index) {
+        var i = 0,
+            indent = 0,
+            padding = '';
+
+        if (node.match(/\{$/) || node.match(/\[$/)) {
+            indent = 1;
+        } else if (node.match(/\}/) || node.match(/\]/)) {
+            if (pad !== 0) {
+                pad -= 1;
+            }
+        } else {
+            indent = 0;
+        }
+
+        for (i = 0; i < pad; i++) {
+            padding += PADDING;
+        }
+
+        formatted += padding + node + '\r\n';
+        pad += indent;
+    });
+    return formatted;
+};
+
+function formatJsonHtml(msg) {
+    var rep = "~";
+    var jsonStr = JSON.stringify(msg, null, rep)
+    var str = "";
+    for (var i = 0; i < jsonStr.length; i++) {
+        var text2 = jsonStr.charAt(i)
+        if (i > 1) {
+            var text = jsonStr.charAt(i - 1)
+            if (rep != text && rep == text2) {
+                str += "<br/>"
+            }
+        }
+        str += text2;
+    }
+    jsonStr = "";
+    for (var i = 0; i < str.length; i++) {
+        var text = str.charAt(i);
+        if (rep == text)
+            jsonStr += "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+        else {
+            jsonStr += text;
+        }
+        if (i == str.length - 2)
+            jsonStr += "<br/>"
+    }
+    return jsonStr;
+}
+
+module.exports = router;
